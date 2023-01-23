8aa1b082a4cdd6b88ec8660e395e9bc26c7a37ec
Jaime A. Araujo <neodingo9@gmail.com>
2023-01-22 19:23:44 -0600

    add compareGuesses function

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ let currentRoundNumber = 1;
 const generateTarget = () =>{
     return Math.floor(Math.random() * 10);
 };
+
+/*Determines which player (human or computer) wins based on which guess is closest to the target.
+If both players are tied, the human user should win.
+ */
+
+const compareGuesses = (humanGuess, computerGuess, secretTarget) =>{
+  if (humanGuess === secretTarget){
+      return true
+  }  else if (computerGuess === secretTarget) {
+      return false
+  }
+};
+
