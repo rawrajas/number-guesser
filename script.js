e109d86ada78c83b7199b902ff930b85ef9696c5
Jaime A. Araujo <neodingo9@gmail.com>
2023-01-22 19:14:12 -0600

    add: generateTarget function

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
-
+// Generates random integer between 0 & 9
+const generateTarget = () =>{
+    return Math.floor(Math.random() * 10);
+};
